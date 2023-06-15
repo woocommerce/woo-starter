d381c4d300e26f617e00356adf3053c5b1eb134e
jamelreid <jnoelreid@gmail.com>
2023-06-15 16:24:45 -0500

    Update FileHelper.php

--- src/traits/FileHelper.php
+++ src/traits/FileHelper.php
@@ -2,6 +2,8 @@
 
 namespace WooStarter\traits;
 
+use WooStarter\App;
+
 trait FileHelper {
 	/**
 	 * Get the original file name without the .twig extension from a Twig template file.
@@ -49,10 +51,26 @@ trait FileHelper {
 		return $relative_path;
 	}
 
-
+	/**
+	 * Check if a path is a valid directory.
+	 *
+	 * @param string $path
+	 * @param string $parent
+	 * @return bool
+	 */
 	function is_valid_directory ( string $path, $parent ): bool {
 		return is_dir( $path ) && ! empty( $this->get_path_relative_to_parent( $path, $parent ) );
 	}
 
-
+	/**
+	 * Get the new file name for a template.
+	 *
+	 * @param string $file_name
+	 * @param string $new_slug
+	 * @return string
+	 */
+	function get_new_file_name( string $file_name, string $new_slug ): string {
+		$original_file_name = $this->get_original_file_name( $file_name );
+		return str_replace( App::getVar( 'default_slug' ), $new_slug, $original_file_name );
+	}
 }
